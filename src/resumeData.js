29061a1227f17d54cc371ab00b86e1c16a406af0
Adrian <70490120+adrianpinon@users.noreply.github.com>
2021-03-13 14:23:42 -0600

    added functional components

---
+++ src/resumeData.js
@@ -0,0 +1,87 @@
+let resumeData = {
+
+    "imagebaseurl":"https://github.com/adrianpinon",
+    "name": "Adrian",
+    "role": "Full Stack Developer",
+    "linkedinId": "Your ID",
+    "roleDescription":"I like to learn and create applications in various parts of software development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+
+    "socialLinks": [
+        {
+            "name": "linkedin",
+            "url": "https://www.linkedin.com",
+            "className": "fa fa - linkedin"
+        },
+        {
+            "name": "github",
+            "url": "https://github.com/adrianpinon",
+            "className": "fa fa - github"
+        },
+
+    ],
+
+    "website": "https://github.com/adrianpinon",
+        "education": [
+            {
+                "UniversityName": "University of Minnesota",
+                "specialization": "Full-Stack Web Development",
+                "MonthOfPassing": "March",
+                "YearOfPassing": "2021",
+                "Achievements": "Some Achievements"
+            },
+            {
+                "UniversityName": "Some University",
+                "specialization": "Some specialization",
+                "MonthOfPassing": "Jan",
+                "YearOfPassing": "2018",
+                "Achievements": "Some Achievements"
+            }
+        ],
+
+    "work": [
+        {
+            "CompanyName": "Some Company",
+            "specialization": "Some specialization",
+            "MonthOfLeaving": "",
+            "YearOfLeaving": "",
+            "Achievements": "Some Achievements"
+        },
+        {
+            "CompanyName": "Some Company",
+            "specialization": "Some specialization",
+            "MonthOfLeaving": "Jan",
+            "YearOfLeaving": "2018",
+            "Achievements": "Some Achievements"
+        }
+    ],
+    "skillsDescription": "Your skills here",
+    "skills": [
+        {
+            "skillname": "HTML5"
+        },
+        {
+            "skillname": "CSS"
+        },
+        {
+            "skillname": "React.js"
+        }
+    ],
+    "portfolio": [
+        {
+            "name": "project1",
+            "description": "mobileapp",
+            "imgurl": "images / portfolio / phone.jpg"
+        },
+        {
+            "name": "project2",
+            "description": "mobileapp",
+            "imgurl": "images / portfolio / project.jpg"
+        },
+        {
+            "name": "project3",
+            "description": "mobileapp",
+            "imgurl": "images / portfolio / project2.png"
+        }
+    ]
+}
+export default resumeData
